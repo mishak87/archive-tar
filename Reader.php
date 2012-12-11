32918434be32709f16dc444174978efecfc48a51
Michal Gebauer <mishak@mishak.net>
2012-12-11 18:33:29 +0100

    Init

---
+++ Reader.php
@@ -0,0 +1,220 @@
+<?php
+
+namespace Mishak\ArchiveTar;
+
+use Exception;
+
+class Reader implements \Iterator {
+
+
+	private $filename;
+
+	public function __construct($filename)
+	{
+		$this->filename = $filename;
+		$this->detectCompression();
+		$this->open();
+	}
+
+	public function __destruct()
+	{
+		$this->close();
+	}
+
+
+	const GZIP = 'gz',
+		BZIP2 = 'bz2',
+		NONE = 'none';
+
+	private $compression;
+
+	private function detectCompression()
+	{
+		if (preg_match('/\.(tar\.gz|tgz)$/', $this->filename)) {
+			$this->compression = self::GZIP;
+		} elseif (preg_match('/\.(tar\.bz2|tbz|tb2)$/', $this->filename)) {
+			$this->compression = self::BZIP2;
+		} elseif (preg_match('/\.tar$/')) {
+			$this->compression = self::NONE;
+		} else {
+			throw new Exceptioln("Unsupported compression '$this->filename'.");
+		}
+	}
+
+
+	private $manipulation = array(
+		self::GZIP => array('gzopen', 'gzclose'),
+		self::BZIP2 => array('bzopen', 'bzclose'),
+		self::NONE => array('fopen', 'fclose'),
+	);
+
+	private $file;
+
+	private function open()
+	{
+		list($open,) = $this->manipulation[$this->compression];
+		$this->file = $open($this->filename, 'rb');
+		if (!$this->file) {
+			throw new \Exception("Cannot open file '$this->filename'.");
+		}
+	}
+
+
+	private function close()
+	{
+		list(, $close) = $this->manipulation[$this->compression];
+		$close($this->file);
+		$this->file = NULL;
+	}
+
+
+	private $record;
+
+	private $index;
+
+	public function current()
+	{
+		return $this->record;
+	}
+
+	public function key()
+	{
+		return $this->index;
+	}
+
+	public function next()
+	{
+		return $this->record = $this->readRecord();
+	}
+
+	public function rewind()
+	{
+		fseek($this->file, 0);
+		$this->next();
+	}
+
+	public function valid()
+	{
+		return $this->record !== NULL;
+	}
+
+
+	private $readContents = TRUE;
+
+	/**
+	 * Sets read mode of file contents.
+	 *
+	 * @param bool|callback $readContents
+	 *	FALSE disables reading of contents
+	 *	TRUE will read all contents and return them with record
+	 *	callback should expects parameters $record, $chunk, $bytesLeft and $bytesRead. Chunk will be of size of buffer or smaller.
+	 */
+	public function setReadContents($readContents = TRUE)
+	{
+		$this->readContents = $readContents;
+	}
+
+	private $buffer = 8195;
+
+	/**
+	 * Sets buffer size for reading file contents
+	 *
+	 * @param int
+	 */
+	public function setBuffer($buffer)
+	{
+		if (0 < $buffer && $buffer <= PHP_INT_MAX) {
+			$this->buffer = $buffer;
+		} else {
+			throw new Exception("Buffer must be greater then 0 and less or equal to " . PHP_INT_MAX . " (PHP_INT_MAX).");
+		}
+	}
+
+
+	const REGULAR = '0',
+		AREGULAR = "\0", // backward compatibility
+		HARDLINK = '1',
+		SYMLINK = '2',
+		CHARACTER = '2',
+		BLOCK = '2',
+		DIRECTORY = '2',
+		FIFO = '2',
+		CONTIGUOUS = '2',
+		GLOBALHEADER = 'g',
+		EXTENDEDHEADER = 'x',
+		LONGLINK = 'K';
+
+	private function readRecord()
+	{
+		$block = $this->readBlock();
+		$this->index++;
+		return $block;
+	}
+
+	const BLOCK_SIZE = 512;
+
+	private function readBlock()
+	{
+		if (feof($this->file)) {
+			return NULL;
+		} else {
+			$header = fread($this->file, self::BLOCK_SIZE);
+			$record = unpack("a100name/a8mode/a8uid/a8gid/a12size/a12mtime/a8checksum/a1type/a100symlink/a6magic/a2version/a32owner/a32group/a8deviceMajor/a8deviceMinor/a155prefix/a12unpacked", $block);
+			$record['filename'] = $unpacked['prefix'] . $unpacked['name'];
+			// convert to decimal values
+			foreach (array('uid', 'gid', 'size', 'mtime', 'checksum') as $key) {
+				$record[$key] = octdec($record[$key]);
+			}
+			if ($record['checksum'] == 0x00000000) {
+				return NULL;
+			} elseif (0 !== strpos($record['magic'], 'ustar')) {
+				throw new \Exception('Unsupported archive type.');
+			}
+
+			$checksum = 0;
+			for ($i = 0; $i < self::BLOCK_SIZE; $i++) {
+				$checksum += 148 <= $i && $i <= 156 ? 32 : ord($header[$i]);
+			}
+			if ($record['checksum'] != $checksum) {
+				throw new \Exception('Archive is corrupted.');
+			}
+
+			$length = $record['size'];
+			if (is_float($length)) {
+				$padding = self::BLOCK_SIZE - fmod($length, self::BLOCK_SIZE);
+			} else {
+				$padding = self::BLOCK_SIZE - $length % self::BLOCK_SIZE;
+			}
+
+			$file['contents'] = NULL;
+			if ($length == 0 && is_callback($this->readContents)) {
+				call_user_func($this->readContents, $record, '', 0, 0);
+			}
+			while ($length > 0) {
+				if ($length > $this->buffer) {
+					$read = $this->buffer;
+					$length -= $this->buffer;
+				} else {
+					$read = $length;
+					$length = 0;
+				}
+
+				if ($this->contents === FALSE) {
+					fseek($this->file, $read, SEEK_CUR);
+				} else {
+					$chunk = fread($this->file, $read);
+					if (is_callback($this->readContents)) {
+						call_user_func($this->readContents, $record, $chunk, $length, $read);
+					} else {
+						$file['contents'] .= $read;
+					}
+				}
+			}
+			if ($padding !== self::BLOCK_SIZE) {
+				fseek($this->file, $padding, SEEK_CUR);
+			}
+			return $record;
+		}
+	}
+
+}
---
+++ readme.md
@@ -0,0 +1,68 @@
+Archive TAR Reader for PHP
+==========================
+
+Simple tool for reading TAR archives in PHP. Supports records with size larger then PHP_INT_MAX (slightly less then 2 GB).
+
+**Supports only USTAR format.** This should be the most used TAR format after 1988.
+
+Gz and bz2 compressions are transparently supported via detection from filename extension.
+
+## Installation
+
+Add to your `composer.json` requirement `"mishak/archive-tar": "dev-master"`.
+
+## Examples
+
+### List all records in archive
+
+```php
+$filename = 'archive.tar';
+$reader = new Mishak\ArchiveTar\Reader($filename);
+$read->setBuffer(PHP_INT_MAX);
+$read->setReadContents(FALSE);
+foreach ($reader as $record) {
+	print_r($record);
+}
+```
+
+### Print all file records with contents
+
+```php
+$filename = 'archive.tar';
+$reader = new Mishak\ArchiveTar\Reader($filename);
+foreach ($reader as $record) {
+	if (in_array($record['type'], array(\Mishak\ArchiveTar\Reader::REGULAR, \Mishak\ArchiveTar\Reader::AREGULAR), TRUE)) {
+		echo $record['filename'], "\n";
+		echo $record['contents'], "\n";
+	}
+}
+```
+
+### Print all file records via function callback
+
+This will produce exactly same output as previous example.
+
+```php
+$filename = 'archive.tar';
+$reader = new Mishak\ArchiveTar\Reader($filename);
+$lastRecord = NULL;
+$read->setReadContents(function ($record, $chunk, $left, $read) use ($lastRecord) {
+	if (!in_array($record['type'], array(\Mishak\ArchiveTar\Reader::REGULAR, \Mishak\ArchiveTar\Reader::AREGULAR), TRUE)) {
+		continue;
+	}
+	if (NULL === $lastRecord || $record['filename'] !== $lastRecord['filename']) {
+		if (NULL !== $lastRecord) {
+			echo "\n";
+		}
+		echo $record['filename'], "\n";
+	}
+	echo $chunk;
+	if (!$left) {
+		echo "\n";
+	}
+}
+});
+foreach ($reader as $record) {
+	// don't mind just walking thru...
+}
+```
